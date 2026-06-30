2c25e917b5c6aa701b773fa667afdee311930ffa
oscarlopez <olaoscar2005@gmail.com>
2026-06-30 19:33:05 +0200

    fix: lazy Resend client initialization to avoid build-time errors

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,12 @@
 import { Resend } from "resend";
 
-export const resend = new Resend(process.env.RESEND_API_KEY!);
+let _client: Resend | null = null;
+
+export function getResend(): Resend {
+  if (!_client) {
+    _client = new Resend(process.env.RESEND_API_KEY!);
+  }
+  return _client;
+}
+
+export const resend = { emails: { send: (...args: Parameters<Resend["emails"]["send"]>) => getResend().emails.send(...args) } };
